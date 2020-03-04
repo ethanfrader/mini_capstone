699b345899df5db7b44cb31d32dc8ea451c54fa6
Ethan Rader <rader.ethan@gmail.com>
2020-03-03 19:25:55 -0600

    created api methods and json for all products as well as the three individuals

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jb"
   end
+
+  def ipa_method
+    @product = Product.find_by(name: "IPA")
+    render "ipa_product.json.jb"
+  end
+
+  def brown_ale_method
+    @product = Product.find_by(name: "Brown Ale")
+    render "ale_product.json.jb"
+  end
+
+  def hefeweizen_method
+    @product = Product.find_by(name: "Hefeweizen")
+    render "hefeweizen_product.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_products" => "products#all_products_method"
+    get "/ipa" => "products#ipa_method"
+    get "/brown_ale" => "products#brown_ale_method"
+    get "/hefeweizen" => "products#hefeweizen_method"
   end
 end
