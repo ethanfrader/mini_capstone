80ee38558b4a102855877e3b5d30799ec0ba08fb
Ethan Rader <rader.ethan@gmail.com>
2020-03-08 11:44:17 -0500

    created models for is_discounted? tax and total

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    discount = false
+    if price < 6
+      discount = true
+    end
+    discount
+  end
+
+  def tax
+    (price * 0.09).round(2)
+  end
+
+  def total
+    price + tax
+  end
 end
