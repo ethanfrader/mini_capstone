a0dd78ebba5c256949ee2a512dad9829b2d0d68b
Ethan Rader <rader.ethan@gmail.com>
2020-03-17 18:30:20 -0500

    now authorizes user in orders via authenticate_user method

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,12 +1,9 @@
 class Api::OrdersController < ApplicationController
-  def index
-    if current_user
-      @orders = current_user.orders
+  before_action :authenticate_user
 
-      render "index.json.jb"
-    else
-      render json: {}, status: :unauthorized
-    end
+  def index
+    @orders = current_user.orders
+    render "index.json.jb"
   end
 
   def create
@@ -15,29 +12,21 @@ class Api::OrdersController < ApplicationController
     calculated_subtotal = product.price * params["quantity"].to_i
     calculated_tax = product.tax * params["quantity"].to_i
     calculated_total = calculated_subtotal + calculated_tax
-    if current_user
-      @order = Order.new(
-        user_id: current_user.id,
-        product_id: params["product_id"],
-        quantity: params["quantity"],
-        subtotal: calculated_subtotal,
-        tax: calculated_tax,
-        total: calculated_total,
-      )
-      @order.save
-      render "show.json.jb"
-    else
-      render json: {}, status: :unauthorized
-    end
+
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params["product_id"],
+      quantity: params["quantity"],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
+    )
+    @order.save
+    render "show.json.jb"
   end
 
   def show
-    if current_user
-      @order = current_user.orders.find_by(id: params["id"])
-
-      render "show.json.jb"
-    else
-      render json: {}, status: :unauthorized
-    end
+    @order = current_user.orders.find_by(id: params["id"])
+    render "show.json.jb"
   end
 end
