22fd0191faef36c0fe7cd7ef43600e07adf509d3
Ethan Rader <rader.ethan@gmail.com>
2020-03-16 20:22:42 -0500

    added subtotal tax and total ability to create method

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -4,6 +4,9 @@ class Api::OrdersController < ApplicationController
       user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
+      subtotal: Product.find_by(id: params["product_id"]).price * params["quantity"].to_i,
+      tax: Product.find_by(id: params["product_id"]).tax * params["quantity"].to_i,
+      total: Product.find_by(id: params["product_id"]).price * params["quantity"].to_i + Product.find_by(id: params["product_id"]).tax * params["quantity"].to_i,
     )
     @order.save
     render "show.json.jb"
