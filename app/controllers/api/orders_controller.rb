f7083e3146290f8d0b9eb391894f7cc128dc0ffc
Ethan Rader <rader.ethan@gmail.com>
2020-03-17 18:22:15 -0500

    orders controller now authorizes user before attempting to show orders

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -5,7 +5,7 @@ class Api::OrdersController < ApplicationController
 
       render "index.json.jb"
     else
-      render json: []
+      render json: {}, status: :unauthorized
     end
   end
 
@@ -15,22 +15,29 @@ class Api::OrdersController < ApplicationController
     calculated_subtotal = product.price * params["quantity"].to_i
     calculated_tax = product.tax * params["quantity"].to_i
     calculated_total = calculated_subtotal + calculated_tax
-
-    @order = Order.new(
-      user_id: current_user.id,
-      product_id: params["product_id"],
-      quantity: params["quantity"],
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
-    )
-    @order.save
-    render "show.json.jb"
+    if current_user
+      @order = Order.new(
+        user_id: current_user.id,
+        product_id: params["product_id"],
+        quantity: params["quantity"],
+        subtotal: calculated_subtotal,
+        tax: calculated_tax,
+        total: calculated_total,
+      )
+      @order.save
+      render "show.json.jb"
+    else
+      render json: {}, status: :unauthorized
+    end
   end
 
   def show
-    @order = current_user.orders.find_by(id: params["id"])
+    if current_user
+      @order = current_user.orders.find_by(id: params["id"])
 
-    render "show.json.jb"
+      render "show.json.jb"
+    else
+      render json: {}, status: :unauthorized
+    end
   end
 end
