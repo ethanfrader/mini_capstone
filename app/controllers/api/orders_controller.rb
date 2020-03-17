ced2c0bc28676a5a929eca9cf6681c0a83ec694b
Ethan Rader <rader.ethan@gmail.com>
2020-03-16 20:32:10 -0500

    added show for orders. doesn't currently limit to current user

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -21,4 +21,10 @@ class Api::OrdersController < ApplicationController
     @order.save
     render "show.json.jb"
   end
+
+  def show
+    @order = Order.find_by(id: params["id"])
+
+    render "show.json.jb"
+  end
 end
