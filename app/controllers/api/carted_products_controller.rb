ce118d19f3f88fce21ca03096b2c48ec150cabf3
Ethan Rader <rader.ethan@gmail.com>
2020-03-19 20:28:51 -0500

    carted_products/index shows only current users carted items

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -1,6 +1,12 @@
 class Api::CartedProductsController < ApplicationController
   before_action :authenticate_user
 
+  def index
+    @carted_products = current_user.carted_products.where(status: "carted")
+
+    render "index.json.jb"
+  end
+
   def create
     #user sends product_id and quantity as params
     @carted_product = CartedProduct.new({
